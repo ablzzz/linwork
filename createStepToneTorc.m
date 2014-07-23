cec75b3278582a4ab90e96da0aab3a9c919a3466
Ashwin Bellur <ashwinbellur@gmail.com>
2014-07-23 16:39:54 -0400

    First go

---
+++ createStepToneTorc.m
@@ -0,0 +1,30 @@
+function R = createStepToneTorc(toneDiff, numOfInst)
+
+%%% toneDiff is the number of bins to skip to create step Tone
+
+
+startFreq = 180; %%% From wav2aud
+endFreq = 7246;
+stepSize = (log(endFreq) - log(startFreq))/128;
+
+startTone = 200 : (5000 - 200)/numOfInst : 5000;
+endTone = startTone*exp(toneDiff*stepSize);
+torc = 
+%%%% creating step tone %%%%
+t = [0 : 1/16384 : 0.5 - 1/16384];
+for j = 1 : length(startTone); 
+	tone1 = sin(2*pi*startTone(j)*t);
+	tone2 = sin(2*pi*endTone(j)*t);
+	toneClean = [tone1 tone2];
+	gabFeatClean(j).val = extractGabFeature(toneClean);
+	featForSpace(:,:,:,k) = gabFeatClean(j).val;
+	k = k + 1;	
+	for k = -10:5:10; 
+		toneTorcSNR = combineSNR(s, toneClean(j).val,k);
+		gabFeatNoisy(j,k).val = extractGabFeature(toneTorcSNR);
+		featForSpace(:,:,:,k) = gabFeatNoisy(j,k).val;
+		k = k + 1;
+	end
+end
+
+
