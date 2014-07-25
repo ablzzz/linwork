357395561ed8e0300c5e41b01c71080608bb73ce
Ashwin Bellur <ashwinbellur@gmail.com>
2014-07-25 13:02:41 -0400

    Simple gab cr rt and rsf extraction -- no line search

--- extractGabFeature.m
+++ extractGabFeature.m
@@ -1,19 +1,22 @@
-
-function [rsf] = extractSpikesFileName(x)
+function [cr rt rsf] = extractSpikesFileName(y)
+    linS = ones(1,8); 
+    flag = 1;
     loadload;
-    bw = 2*ones(1,8);
-    wt = ones(1,8);
-    paras = [10 4 0 0];;
+    paras(1)=10;
+    paras(2)=4;
+    paras(4)=0;
     rv=2.^[1:1:5];
     sv=2.^[-2:1:3];
-    x=unitseq(x);
-    y=wav2aud(x,paras);
-    %y = x;
-    [m n] = size(bw);
-    for j = 1 : m
-    	gab_filters = create_filters_bf(paras,rv,sv, bw(j,:), wt);
-    	cr=aud2cor_bf(y', gab_filters);
-    	rt = squeeze(sum(abs(cr), 4));
-    	rsf = mean(squeeze(sum(abs(cr), 3)),4);
-   end
+    %x = x();
+    %x=unitseq(x);
+    %fs = 16000;
+    %y=wav2aud(x,paras);
+    [m n] = size(linS)
+  for j = 1 : m
+    gab_filters = create_filters_ashwintilt(paras, linS, flag);
+    cr=aud2cor_bf(y', gab_filters);
+    cr = abs(cr);
+    rt = squeeze(sum(abs(cr), 4));
+    rsf = mean(squeeze(sum(abs(cr), 3)),4);
+  end
    
